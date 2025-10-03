2a61d953bff61e2e0659fe28d57d9172d469422e
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-10-03 21:04:29 +0600

    [version]: set to `0.1.1`;

--- accelera/__init__.py
+++ accelera/__init__.py
@@ -13,7 +13,7 @@ from .subprocess_engine import SubprocessMatrixEngine
 from .config import setup_logging, config
 from . import interceptor
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = [
     "MatrixEngine", 
     "SubprocessMatrixEngine",
